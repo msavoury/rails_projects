05602e02448a93c5517339aca73acdde51c7c068
marcos savoury <msavoury@carecloud.com>
2017-07-25 18:58:18 +0000

    Adding sample app

---
+++ sample_app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = 'f65d117fca305234c11fcdbb92aaf603ebc201818bfa730b7e2aae6f744c21ff02de9c0024eef85838dc4b8b72b8f0c57d47bef96bacaf0a8d3bbdc8843eb4c5'
