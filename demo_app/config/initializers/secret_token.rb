3bc79916b5f19ef94e1df129684108331036a766
marcos savoury <msavoury@carecloud.com>
2017-07-25 17:58:16 +0000

    initial commit

---
+++ demo_app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = 'd5deb34483af78e581f6d8dff1fe88f1e21c846d5ae4179363874fb0e8502191a085f12f0236552aa7c1cdf2c9b6aab5aad4839ad305520e584f0ad21b5ef559'
---
+++ first_app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '12604e75f167f9055030c73c0fa82aab929a9b1d08fa958d363969df780373364322e2191781e55fbe8f4504bdc14c06e91f4e8fa5581c3faec606226b3aa9ab'
